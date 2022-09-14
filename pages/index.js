95fd3780fb6928b914ccd706c1fd5a3eeaa60899
klklsys <jiazhouplus@gmail.com>
2022-09-14 15:29:27 +0900

    severSide

--- pages/index.js
+++ pages/index.js
@@ -1,90 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image';
-import styles from '../styles/Home.module.css'
+import React, { useState, useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
-// get data from serverSide 
-
-// export async function getServerSideProps(){
-//   const resp = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
-//   return{
-//     props:{
-//       pokemon:await resp.json(),
-//     }
-//   }
-// }
-
-
-
-// export default function Home({pokemon}) {
-//   // const [pokemon, setPokemon] = useState([]);
-//   // useEffect(() => {
-//   //   async function getPokemon() {
-//   //     const resp = await fetch('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
-//   //     setPokemon(await resp.json());
-//   //   }
-//   //   getPokemon();
-//   // }, []);
-//   return (
-//     <>
-//       <Head>
-//         <title>Pokemon List Test</title>
-//       </Head>
-//       <div className={styles.grid}>
-//         {pokemon.map((pokemon) => (
-//           <div className={styles.card} key={pokemon.id}>
-//             <Link href={`/pokemon/${pokemon.id}`}>
-//               <a>
-//                 <img
-//                   src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
-//                   alt={pokemon.name}
-//                   className={styles.card_image}
-//                 />
-//                 <h3 className={styles.title}>{pokemon.name}</h3>
-//               </a>
-//             </Link>
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   )
-// }
-
-
-
-export default function Home () {
-  const[pokemon, setPokemon]= useState([]);
-  useEffect(()=>{
-    async function getPokemon (){
-      const resp = await fetch ('https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json');
-      setPokemon(await resp.json());
-    }
-    getPokemon();
-  },[])
-  return(
+export async function getServerSideProps() {
+  const resp = await fetch(
+    "https://jherr-pokemon.s3.us-west-1.amazonaws.com/index.json"
+  );
+  return {
+    props: {
+      pokemon: await resp.json(),
+    },
+  };
+}
+export default function Home({ pokemon }) {
+  return (
     <>
-    <Head>Pokemon Grid</Head>
-    <div className={styles.grid}>
-      {pokemon.map((pokemon)=>(
-        <div className={styles.card} key={pokemon.id}>
-          <Link href={`/pokemon/${pokemon.id}`} >
-            <a>
-              <img src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
-              alt={pokemon.name}
-           className={styles.card_image}/>
-            <h3 className={styles.title}>{pokemon.name}</h3>
-
-
-            </a>
-        </Link>
-      
-        </div>
-      ))}
-
-    </div>
+      <Head>Pokemon Grid</Head>
+      <div className={styles.grid}>
+        {pokemon.map((pokemon) => (
+          <div className={styles.card} key={pokemon.id}>
+            <Link href={`/pokemon/${pokemon.id}`}>
+              <a>
+                <img
+                  src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
+                  alt={pokemon.name}
+                  className={styles.card_image}
+                />
+                <h3 className={styles.title}>{pokemon.name}</h3>
+              </a>
+            </Link>
+          </div>
+        ))}
+      </div>
     </>
-  )
-
-
+  );
 }
--- pages/pokemon/[id].js
+++ pages/pokemon/[id].js
@@ -4,68 +4,18 @@ import Head from "next/head";
 import Link from "next/link";
 import styles from "../../styles/Details.module.css";
 
-// export async function getServerSideProps({params}){
-//     const resp = await fetch (`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`);
-//     return{
-//         props:{
-//             pokemon:await resp.json(),
-//         },
-//     };
-// // }
-// export default function Details(){
-//     // 定义路径
-//     const {query:{ id }, } = useRouter();
-// // 用useState更新数据，UseEffect抓取数据
-//     const [pokemon, setPokemon] = useState(null);
+export async function getServerSideProps({ params }) {
+  const resp = await fetch(
+    `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${params.id}.json`
+  );
+  return {
+    props: {
+      pokemon: await resp.json(),
+    },
+  };
+}
 
-//     useEffect(() => {
-//         async function getPokemon(){
-//             const resp = await fetch (`https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`);
-//             setPokemon(await resp.json());
-//         }
-//         if(id){
-//             getPokemon();
-//         }
-//     },[id]);
-//     if(!pokemon){
-//         return null;
-//     }
-//     return(
-//     <div className={styles.container}><Head><titile>{pokemon.name}</titile></Head>
-//     <div><Link href="/"><a>Back to Home</a></Link></div>
-//     <div className={styles.layout}>
-//         <div>
-//             <img className={styles.picture}
-//             src={`https://jherr-pokemon.s3.us-west-1.amazonaws.com/${pokemon.image}`}
-//             alt={pokemon.name.english} />
-//         </div>
-//         <div><p>{pokemon.name}</p></div>
-//         <div>{JSON.stringify(pokemon)}</div>
-//     </div>
-//         </div>
-//     )
-// }
-
-export default function Details() {
-  const {
-    query: { id },
-  } = useRouter();
-  const [pokemon, setPokemon] = useState(null);
-  useEffect(() => {
-    async function getPokemon() {
-      const resp = await fetch(
-        `https://jherr-pokemon.s3.us-west-1.amazonaws.com/pokemon/${id}.json`
-      );
-      setPokemon(await resp.json());
-    }
-    if (id) {
-      getPokemon();
-    }
-  }, [id]);
-  if (!pokemon) {
-    return null;
-  }
-
+export default function Details({ pokemon }) {
   return (
     <>
       <Head>
